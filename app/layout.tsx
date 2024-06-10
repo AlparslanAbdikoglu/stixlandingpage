aaaf46d66b45bbe2e43779239e062bfbf1e697f7
Alpii <alparslanabdik@gmail.com>
2024-06-10 12:56:04 +0200

    stats

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next"; // Import the SpeedInsights component
 import "./globals.css";
 
 
@@ -21,6 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+      <SpeedInsights /> {/* Add the SpeedInsights component */}
         {children}
         </body>
     </html>
